2cd2d6d05279984ae43cdfaa5c6dbfbf66af960d
motsan <motsan06@gmail.com>
2023-12-09 01:12:41 +0330

    Implementation of the News class

---
+++ app/src/main/java/com/newsapp/News.kt
@@ -0,0 +1,13 @@
+package com.newsapp
+
+data class News(
+    var titel: String,
+    var description: String,
+    var date: String,
+    var writer: String,
+    var imageUri: String,
+    var linkUrl: String
+) {
+
+}
+//POJO
