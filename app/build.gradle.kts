563ec90360cc11e48a147bfe0d215f9b8f070cb1
motsan <motsan06@gmail.com>
2023-12-09 03:45:20 +0330

    add json and retrofit library and edit news class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,10 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
--- app/src/main/java/com/newsapp/News.kt
+++ app/src/main/java/com/newsapp/News.kt
@@ -1,8 +1,15 @@
 package com.newsapp
 
 data class News(
-    var titel: String,
-    var description: String,
+
+    val author: Any,
+    val content: Any,
+    val description: Any,
+    val publishedAt: String,
+    val source: Source,
+    val title: String,
+    val url: String,
+    val urlToImage: Any,
     var date: String,
     var writer: String,
     var imageUri: String,
